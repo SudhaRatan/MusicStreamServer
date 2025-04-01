c444162388f383b21271fdc7d4b38ba7cf12e401
SudhaRatan <rattankumar2511@gmail.com>
2025-04-01 12:46:53 +0530

    working now

--- functions/socketFunctions.js
+++ functions/socketFunctions.js
@@ -4,8 +4,9 @@ const ytdl = require('ytdl-core');
 const getSong = async (song) => {
   var list = await youtubesearchapi.GetListByKeyword(song, false, 5);
   let e = await ytdl.getInfo(`http://www.youtube.com/watch?v=${list.items[0].id}`);
-  let audioFormat = ytdl.chooseFormat(e.formats, { quality: 'highestaudio', filter: 'audioonly' });
-  var url = audioFormat.url;
+  // let audioFormat = ytdl.chooseFormat(e.formats, { quality: 'highestaudio', filter: 'audioonly' });
+  console.log(e.formats.filter(i => i.mimeType.includes('video/mp4;') && i.qualityLabel == "360p" && i.audioBitrate))
+  var url = e.formats.filter(i => i.mimeType.includes('video/mp4;') && i.qualityLabel == "360p" && i.audioBitrate)[0].url;
   var title = e.videoDetails.title;
   var thumbnails = e.videoDetails.thumbnails;
   var thumbnail = e.videoDetails.thumbnails[thumbnails.length - 1];
